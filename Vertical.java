4f8e84fb5c99d83c566559061ba193461a06224c
Antony Syriac <unbleavabl@gmail.com>
2017-01-30 13:29:12 +0530

    First commit

---
+++ Vertical.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Vertical
+{
+	public static void main(String[] args) 
+	{
+		Scanner u_inp = new Scanner(System.in);
+		int n;
+		System.out.print("Enter n :");
+		n = u_inp.nextInt();
+		for(int i = 0; i < n ; i++)
+		{
+			System.out.println("*");
+		}
+
+	}
+}
